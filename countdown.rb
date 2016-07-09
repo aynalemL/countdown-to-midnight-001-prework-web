532587d9fea4fa3ccdd3efd4208c6d27ae6e2170
Aynalem Lenjiso <aynalem.lenjiso@gmial.com>
2016-07-09 18:11:47 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(num_sec)
+  counter = 0
+  while counter < num_sec
+    puts "#{num_sec} SECOND(S)!"
+     num_sec -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(countdown)
+  counter = 0
+  while counter < countdown
+    puts "#{countdown} SECOND(S)!"
+    sleep(1)
+     countdown -= 1
+  end
+    return "HAPPY NEW YEAR!"
 end
